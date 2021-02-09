b61215ed741ca6e3172a4b47d4be022c4c3723d3
Jan-Erik Läänesaar <70939499+Sproker@users.noreply.github.com>
2021-02-09 08:56:50 +0200

    Update Ascii.java

--- w01_funktsioon/Ascii.java
+++ w01_funktsioon/Ascii.java
@@ -1,24 +1,28 @@
 //Scanner klassi importimine java.utilist    
 import java.util.Scanner;
-public class Ascii   
+public class Ascii2   
 {  
-    public static void main(String[] args)   
-    {  
+    public static void sisend() {
     //Scanner objekti deklareerimine kasutajalt sisendi saamiseks
     Scanner sc = new Scanner(System.in);
-
-    System.out.println("Sisestage tähemärk, mida soovite Asciisse teisendada:");   
+    System.out.println("Sisestage tahemark, mida soovite Asciisse teisendada:");   
     System.out.println("");
-
+    
     /*Scanneriga kasutajalt sisendi püüdmine, next() funk. tagastab kasutaja 
     sisestatud sõna/märgi stringina ning charAt(0) tagastab esimese tähemärgi tolles stringis*/
     char ch = sc.next().charAt(0);
+    
     //Sulgeme scanner objekti
     sc.close();
-    System.out.println(""); 
-
+    System.out.println("");
+    
     //Sisendi teisendamine Ascii väärtuseks ning selle kasutajale kuvamine 
     int ascii = (int) ch;  
-    System.out.println("Tähemärgi " + ch + " ascii väärtus on: " + ascii);   
+    System.out.println("Tahemargi " + ch + " ascii vaartus on: " + ascii); 
+    }   
+    public static void main(String[] args)   
+    {
+    //Kutusume loodud funktsiooni välja
+    sisend();
     }  
 } 
